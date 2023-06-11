a2824b61f039a9ead3bc527daceb49c798c1b574
Palash A <palash.agarwal.96@gmail.com>
2023-06-10 23:14:12 -0400

    complete landing (minus heart)

--- src/components/molecules/WelcomeScreen.tsx
+++ src/components/molecules/WelcomeScreen.tsx
@@ -2,25 +2,39 @@ import { HStack, Heading, Spacer, Text, VStack } from '@chakra-ui/react';
 import EnterSiteButton from '@/components/atoms/EnterSiteButton';
 import ConnectWalletButton from '@/components/atoms/ConnectWalletButton';
 
+function LandingHeader({ }) {
+
+  return (
+    <Heading 
+      w='full'
+      pt={8} px={12}
+      color='focus' 
+      opacity={0.7}
+      fontSize='6xl'
+      textAlign='left'
+      position='absolute'
+      fontFamily='displayFitted'
+    >
+      heart-felt <br />
+      productions <br />
+      inc.
+    </Heading>
+  );
+}
+
+function BeatingHeart({ })  {
+  
+  return (
+    <></>
+  );
+}
+
 
 export default function WelcomeScreen({ }) {
 
   return (
     <VStack spacing={3} h='full' w='full'>
-      <Heading 
-        w='full'
-        pt={8} px={12}
-        color='focus' 
-        opacity={0.7}
-        fontSize='6xl'
-        textAlign='left'
-        position='absolute'
-        fontFamily='displayFitted'
-      >
-        heart-felt <br />
-        productions <br />
-        inc.
-      </Heading>
+      <LandingHeader />
       <Spacer />
       <EnterSiteButton />
       <HStack spacing={1}>
@@ -28,6 +42,7 @@ export default function WelcomeScreen({ }) {
         <ConnectWalletButton />
       </HStack>
       <Spacer />
+      <BeatingHeart />
     </VStack>
   );
 }
